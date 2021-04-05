3601d64e88ab5eff7e4e41261dc75450240bae5f
Eder Sant'ana <edersana@gmail.com>
2021-04-05 15:26:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+from art import logo
+from random import randint
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+print(logo + "\n")
+
+#Function to check user's answer againt actual check_answer
+def check_answer(guess, answer, turns):
+  """checks answer against guess. 
+  Returns the number of turns remaining"""
+  if guess > answer:
+    print("Too high.")
+    return turns -1
+  elif guess < answer:
+    print("Too low.")
+    return turns -1
+  else:
+    print(f"You got it! The answer was {answer}.")  
+
+#Make function to set difficulty
+def set_difficulty():
+  level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  if level == "easy":
+    return EASY_LEVEL_TURNS
+  else:
+    return HARD_LEVEL_TURNS  
+
+def game():
+  random_number = randint(1, 100)
+  print(random_number)
+  # Allow the player to submit a guess for a number between 1 and 100.
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100.")
+
+  turns = set_difficulty()
+  guess = 0
+  while guess != random_number:
+    print(f"You have {turns} attempts.")
+    guess = int(input("Guess a number between 1 and 100: "))
+    turns = check_answer(guess, random_number, turns)
+
+    if turns == 0:
+      print("You've run of guesses, you lose")
+      return
+game()
+
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+
+
